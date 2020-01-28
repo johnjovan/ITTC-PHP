89cfe9b68a9833180198944dab0a7a9deaf13b3f
johnjovan <54874802+johnjovan@users.noreply.github.com>
2020-01-28 09:31:59 +0800

    PHP Variable

---
+++ example01.php
@@ -0,0 +1,18 @@
+<?php
+    echo "Hello PHP <br> <hr>";
+    
+    echo "Hello World";
+
+    // Variable
+    // Operator +,-,*,/
+    $a = 10;
+    $b = 20;
+    $c = 'School';
+
+    echo "<br>";
+
+    echo $a + $b ."<br>";
+    echo $c . " start time is " . $a . "am!<br>";
+
+    echo $a % $b;
+?>
