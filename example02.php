8a3523c1c89743b9f96231ae08bb5af4a369a913
johnjovan <54874802+johnjovan@users.noreply.github.com>
2020-01-28 09:34:49 +0800

    PHP Comparision Operator

---
+++ example02.php
@@ -0,0 +1,13 @@
+<?php
+
+    // Comparision Operator
+    // True => 1. False => 0
+    // == , === , !(not) , <>
+    // < , > , <= , >=
+
+    $a = 123;
+    $b = 123;
+
+    echo $a <> $b;
+
+?>
