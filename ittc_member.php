26a09ac3afe6752125efefbfeb4d4b9cee52535f
johnjovan <emorock0925@gmail.com>
2020-02-06 11:39:16 +0800

    ITTC Member

--- ittc_member.php
+++ ittc_member.php
@@ -6,13 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>IT TRAINING CENTER</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <style>
         header {
             margin: 0;
             padding: 0;
             text-align: center;
         }
-
 
     </style>
 </head>
@@ -29,39 +29,41 @@
       $item = [];
     }
       ?>
-
+    <div class="container">
     <header>
         <div id="title">
             <h1>IT TRAINING CENTER [APPLICATION FORM]</h1>
         </div>
     </header>
     <hr>
+    </div>
     <div class="container">
-        <form method="post" action="ittc_action.php">
+        <form method="post" action="ittc_action.php" enctype="multipart/form-data">
             <b>I.PERSONAL DATA:</b><br>
             <label>Picture Upload:
-                <input type="file">
+                <input type="file" name="Image">
             </label><br>
             <label>Name:</label>
-            <input type="text" name="Name" id="Name" placeholder="Full Name"><br>
+            <input type="text" name="Name" id="Name" placeholder="Full Name" required><br>
 
             <label>Cellphone:</label>
             <input type="number" name="Number" id="Number"><br>
 
             <label>Address:</label>
-            <input type="text" name="Address" id="Address"><br>
+            <input type="text" name="Address" id="Address" size="60"><br>
 
             <label>Birthday:</label>
-            <input type="date" name="Birthday" id="Birthday">
+            <input type="date" name="Birthday" id="Birthday"> 
+            <!-- &nbsp;&nbsp;&nbsp;&nbsp;&gt;&lt;&quot;&#13; -->
 
             <label>Age:</label>
-            <input type="age" name="Age" id="Age"><br>
+            <input type="number" name="Age" id="Age" min="0" max="100"><br>
 
             <label>Gender - Man</label>
-            <input type="radio" name="Gender" id="Man" value="man">
+            <input type="radio" name="Gender" id="Man" value="Man">
 
             <label> Woman</label>
-            <input type="radio" name="Gender" id="Woman" value="woman"><br>
+            <input type="radio" name="Gender" id="Woman" value="Woman"><br>
 
             <label>Height:</label>
             <input type="number" name="Height" id="Height">Cm
@@ -160,6 +162,11 @@
     </div>
     <br><br><br><br><br><br><br>
 
+
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>
 
 </html>
